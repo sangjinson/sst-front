19966f5b66bfd0500c3591259e336c04f09616d6
mojitt <jtkim0108@gmail.com>
2026-04-23 11:08:05 +0900

    vite.config 변경

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url';
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-})
-
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
