ace0c5b6f07de0cc355f4ce67c6b02eb11b0d266
sangjinson <rhqeh5tp@gmail.com>
2026-04-23 17:09:52 +0900

    먹거리 수정

--- src/pages/area/AreaBaseTemplate.jsx
+++ src/pages/area/AreaBaseTemplate.jsx
@@ -10,7 +10,7 @@ const AreaBaseTemplate = () => {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
             <Header />
-            <main style={{ flex: 1, backgroundColor: '#f9f9f9', padding: '40px 50px' }}>
+            <main style={{ flex: 1, backgroundColor: '#f9f9f9'}}>
                 <Outlet />
             </main>
             <Footer />
