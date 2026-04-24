71588cbde0e6b2253539a9bd34e33a5d1f40720b
sojeu <sjo8080@naver.com>
2026-04-24 12:00:27 +0900

    볼거리

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -10,7 +10,6 @@ import AdminDashboard from '@pages/admin/Dashboard';
 import Unauthorized from '@pages/error/Unauthorized';
 import MyPage from '@pages/user/MyPage';
 
-import SeeList from '@pages/area/see/List';
 
 import Notice from "@pages/customersupport/Notice";
 import NoticeDetail from "@pages/customersupport/NoticeDetail";
@@ -28,8 +27,6 @@ const AppRoutes = () => {
       <Route path="/" element={<LandingPage />} />
       <Route path="/unauthorized" element={<Unauthorized />} />
       
-      {/* 볼거리 페이지 */}
-      <Route path="/see" element={<SeeList />} />
 
       {/* 일반 사용자 권한 (USER, ADMIN) */}
       <Route element={<ProtectedRoute allowedRoles={['USER', 'ADMIN']} />}>
